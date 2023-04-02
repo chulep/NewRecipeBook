62c233537c812e776688d0057357ac89aa0aec36
Pavel <80887778+chulep@users.noreply.github.com>
2023-04-02 08:28:44 +0300

    done

--- NewRecipeBook/Sources/BookScene/View/BookCollectionViewCell.swift
+++ NewRecipeBook/Sources/BookScene/View/BookCollectionViewCell.swift
@@ -11,13 +11,13 @@ class BookCollectionViewCell: UICollectionViewCell {
     
     static let identifire = "bookCell"
     
+    private var isSetImage = false
+    
     //MARK: - UI Elements
     
     private var imageView: UIImageView = {
-        $0.tintColor = ColorHelper.gray
-        $0.backgroundColor = ColorHelper.lightGray
-        $0.layer.cornerRadius = 10
-        $0.clipsToBounds = true
+        $0.tintColor = ColorHelper.lightGray
+        $0.backgroundColor = .clear
         return $0
     }(UIImageView())
     
@@ -26,6 +26,11 @@ class BookCollectionViewCell: UICollectionViewCell {
         $0.numberOfLines = 2
         return $0
     }(UILabel())
+    
+    private let lineView: UIView = {
+        $0.backgroundColor = ColorHelper.gray
+        return $0
+    }(UIView())
     
     //MARK: - LayoutSubviews
     
@@ -41,23 +46,45 @@ class BookCollectionViewCell: UICollectionViewCell {
         clipsToBounds = true
         backgroundColor = ColorHelper.babackgroundGrey
         
+        addSubview(lineView)
         addSubview(imageView)
         addSubview(titleLabel)
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate(createImageViewConstraints(isSetImage: isSetImage))
         
         NSLayoutConstraint.activate([
-            imageView.topAnchor.constraint(equalTo: topAnchor, constant: 5),
-            imageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 5),
-            imageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -5),
-            imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor),
-            
-            titleLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor),
+            titleLabel.topAnchor.constraint(equalTo: topAnchor, constant: bounds.width),
             titleLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 7),
             titleLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -7),
-            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor)
+            titleLabel.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            lineView.heightAnchor.constraint(equalToConstant: 1),
+            lineView.bottomAnchor.constraint(equalTo: titleLabel.topAnchor),
+            lineView.leftAnchor.constraint(equalTo: leftAnchor),
+            lineView.rightAnchor.constraint(equalTo: rightAnchor)
         ])
+    }
+    
+    private func createImageViewConstraints(isSetImage: Bool) -> [NSLayoutConstraint] {
+        if isSetImage {
+            return [
+                imageView.topAnchor.constraint(equalTo: topAnchor),
+                imageView.leftAnchor.constraint(equalTo: leftAnchor),
+                imageView.rightAnchor.constraint(equalTo: rightAnchor),
+                imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+            ]
+        } else {
+            return [
+                imageView.topAnchor.constraint(equalTo: topAnchor, constant: 25),
+                imageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 25),
+                imageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -25),
+                imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor)
+            ]
+        }
     }
     
     //MARK: - Set data
@@ -65,12 +92,12 @@ class BookCollectionViewCell: UICollectionViewCell {
     func setData(title: String?, imageData: Data?) {
         titleLabel.text = title
         if imageData != nil {
+            isSetImage = true
             imageView.contentMode = .scaleAspectFill
             imageView.image = UIImage(data: imageData!)
         } else {
             imageView.contentMode = .scaleAspectFit
-            imageView.image = UIImage(systemName: "camera")
+            imageView.image = (UIImage(named: "camera")?.withRenderingMode(.alwaysTemplate))
         }
     }
-    
 }
